6856f59fdc50ef53854c5e20d0dddbdda2b61c48
Chris Lee <clee@mg8.org>
2017-03-26 04:26:30 -0600

    Update from 'time' to 'chrono'
    Also, use more standard traits, and clean up some of the code.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-extern crate libc;
-extern crate time;
+extern crate chrono;
 extern crate walkdir;
 
-use walkdir::WalkDir;
-use std::path::Path;
-use std::env;
-use std::collections::BTreeMap;
-use std::os::unix::fs::MetadataExt;
-use std::iter::repeat;
-use time::*;
 use std::io;
+use std::env;
+use std::error::Error;
+use std::collections::BTreeMap;
+use std::iter::repeat;
+use std::time::UNIX_EPOCH;
+use std::path::Path;
+
+use chrono::{NaiveDateTime, Datelike};
+use walkdir::WalkDir;
 
 fn walk(directory: &Path, years: &mut BTreeMap<i32, u64>) -> io::Result<()> {
     for entry in WalkDir::new(&directory) {
-        let meta = entry?.path().metadata()?;
+        let meta = entry?
+            .path()
+            .symlink_metadata()?;
 
         if !meta.is_file() {
             continue;
         }
 
-        let year = 1900 + at_utc(Timespec::new(meta.mtime(), meta.mtime_nsec() as i32)).tm_year;
-
-        *years.entry(year).or_insert(0) += 1;
+        if let Ok(seconds) = meta.modified()?.duration_since(UNIX_EPOCH) {
+            let year = NaiveDateTime::from_timestamp(seconds.as_secs() as i64, 0).date().year();
+            *years.entry(year).or_insert(0) += 1;
+        }
     }
     Ok(())
 }
@@ -31,30 +35,19 @@ fn main() {
     let mut years: BTreeMap<i32, u64> = BTreeMap::new();
 
     let directory = match args.len() {
-        2 => {
-            Path::new(&args[1])
-        },
-        _ => {
-            panic!("usage: agealyzer </path/to/directory>")
-        }
+        2 => Path::new(&args[1]),
+        _ => panic!("usage: agealyzer </path/to/directory>"),
     };
 
-    if let Err(_) = walk(&directory, &mut years) {
-      panic!("failure walking");
+    if let Err(e) = walk(&directory, &mut years) {
+        panic!("failure walking: {}", e.description());
     }
 
-    let zero = 0u64;
-    let max_hits = match years.values().max() {
-        Some(m) => m,
-        None => &zero,
-    };
+    let max_hits = years.values().max().expect("should be at least one int");
     for y in years.keys() {
-        let hits = match years.get(&y) {
-            Some(h) => h,
-            None => &zero,
-        };
-        let p = (40.0 * *hits as f64) / *max_hits as f64;
+        let hits = years.get(&y).expect("should be a value here");
+        let p = (60.0 * *hits as f64) / *max_hits as f64;
         let pf = repeat("=").take(p as usize).collect::<String>();
-        println!("{0}: |{1: <40}| ({2})", y, pf, hits);
+        println!("{0}: |{1: <60}| ({2})", y, pf, hits);
     }
 }
